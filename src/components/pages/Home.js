be7b5a11235b641c4b8620fba7c565dc39c79edf
Nandini Sahni <nandinisahni1504@gmail.com>
2021-02-03 12:09:55 +0530

    abc

---
+++ src/components/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Cards from '../Cards';
+import Footer from '../Footer';
+// import './App.css';
+import HeroSection from '../HeroSection';
+
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+      
+    </>
+  );
+}
+
+export default Home;
